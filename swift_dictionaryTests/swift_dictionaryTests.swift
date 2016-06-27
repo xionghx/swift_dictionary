2563f7015221f9dd5eb121c8b5753da126428e5e
xionghx <xionghuanxin@hotmail.com>
2016-06-27 09:06:36 +0800

    original

---
+++ swift_dictionaryTests/swift_dictionaryTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_dictionaryTests.swift
+//  swift_dictionaryTests
+//
+//  Created by xionghuanxin on 6/27/16.
+//  Copyright © 2016 xionghuanxin. All rights reserved.
+//
+
+import XCTest
+@testable import swift_dictionary
+
+class swift_dictionaryTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
